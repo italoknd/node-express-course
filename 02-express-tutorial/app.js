79913466dba4fca0b2fee57f3ba9dfd9c4ee362b
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-05-10 15:57:21 -0400

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http
+  .createServer((req, res) => {
+    console.log("server running!");
+    res.end("<h1>Everything working!</h1>");
+  })
+  .listen(5000);
