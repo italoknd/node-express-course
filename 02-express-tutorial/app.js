a7493179de7c08bfd2fd163ad4c68e395f6c10eb
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-05-10 18:36:37 -0400

    montando o servidor

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 const server = http
   .createServer((req, res) => {
-    console.log("server running!");
-    res.end("<h1>Everything working!</h1>");
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Everything working!</h1>");
+    res.end();
   })
   .listen(5000);
