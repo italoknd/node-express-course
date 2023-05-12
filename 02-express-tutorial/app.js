9ae1dab8ff3f335beabee019887867d43d46e0f8
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-05-12 13:21:22 -0400

    getting resources from another files (js, css, img)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,45 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+//content
+//1 - home
+const homeHTML = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeLogo = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+//2 - contants
+const contatsHTML = readFileSync("./sections/contats.html");
 
 const server = http
   .createServer((req, res) => {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>Everything working!</h1>");
-    res.end();
+    const url = req.url;
+
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(homeHTML);
+      res.end();
+    } else if (url === "/styles.css") {
+      res.writeHead(200, { "content-type": "text/css" });
+      res.write(homeStyles);
+      res.end();
+    } else if (url === "/logo.svg") {
+      res.writeHead(200, { "content-type": "image/svg+xml" });
+      res.write(homeLogo);
+      res.end();
+    } else if (url === "/browser-app.js") {
+      res.writeHead(200, { "content-type": "text/javascript" });
+      res.write(homeLogic);
+      res.end();
+    } else if (url === "/contats") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(contatsHTML);
+      res.end();
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>This page does not exists!</h1>");
+      res.end();
+    }
   })
-  .listen(5000);
+  .listen(5000, () => {
+    console.log("project running on: http://localhost:5000");
+  });
