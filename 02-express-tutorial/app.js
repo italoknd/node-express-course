f37438377ccfda4bc941a84d40b74bdcb1b9d6c7
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-05-12 14:16:15 -0400

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -13,7 +13,8 @@ const contatsHTML = readFileSync("./sections/contats.html");
 const server = http
   .createServer((req, res) => {
     const url = req.url;
-
+    //we can also add more type of files to be sent to the user's browser, we can send scripts, css text, logos and so on... (example below)
+    //this happens because we reference this files in our html file but the same has to be sent individually
     if (url === "/") {
       res.writeHead(200, { "content-type": "text/html" });
       res.write(homeHTML);
