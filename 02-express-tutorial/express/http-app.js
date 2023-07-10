8bdd108dbc7022df07a10d86c5a3dc748df3c670
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2023-07-09 20:01:23 -0400

    learning about express status code and send method

--- 02-express-tutorial/express/http-app.js
+++ 02-express-tutorial/express/http-app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>This is a home page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>This is an about page</h1>");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send('<h1>Resource not found</h1>')
+});
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`server running on: http://localhost:${PORT}`);
